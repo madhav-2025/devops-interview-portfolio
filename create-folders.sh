54b215c7f1bee13fdc3e50d74eff585a20b28e22
Madhav Jha <jhamadhav2025@gmail.com>
2025-08-01 15:47:37 +0530

    Added folder structure using create-folders.sh

---
+++ create-folders.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+mkdir linux-command-revision
+mkdir shell-scripting-practice
+mkdir git-practice-and-workflows
+mkdir jenkins-pipeline-demo
+mkdir devops-mini-project-html-deploy
+mkdir docker-nginx-demo
+mkdir kubernetes-microservice-deploy
+
